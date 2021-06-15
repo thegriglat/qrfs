0a582bccc10869c14c1bfa4bd6a2597e3ebe6d97
Grigory Latyshev <thegriglat@gmail.com>
2021-06-15 14:10:12 +0300

    Use svg

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,22 @@ import {
   Switch
 } from "react-router-dom";
 
-import {toDataURL, toString} from 'qrcode';
-import React, {Component} from 'react';
+import {toString} from 'qrcode';
+import React from 'react';
 
 class QRC extends React.Component {
   constructor(props){
     super(props);
-    console.log(props)
     this.state = {qr: "", text: props.location.pathname.slice(1)};
   }
 
   componentDidMount(){
     toString(this.state.text, {type: "svg"}, (err, url) => {
       this.setState({qr: url});
-      console.log(url);
     })
   }
  
   render(){
-    // return  <img width="100%" src={this.state.qr} alt="qr"/>;
     return <span dangerouslySetInnerHTML={{__html: this.state.qr}} />;
   } 
 }
